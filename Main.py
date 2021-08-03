8651957b1e4655ec8abd279cdb1d6b63ab57923d
YDeltagon <Git@YDeltagon.fr>
2021-08-03 10:27:45 +0200

    V0.3.0

--- Main.py
+++ Main.py
@@ -2,14 +2,19 @@
 import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-### First Dict
-first_unlock = {
-    "crea_date": "2021-08-02",
-    "creator": "YDeltagon",
-    "social": ["Twitch : @YDeltagon", "Twitter : @YDeltagon"]
+### First Dict (not use)
+owner = {
+    "crea_date":"2021-08-02",
+    "creator":" YDeltagon",
+    "Twitch""twt":" @YDeltagon", 
+    "Twitter":" @YDeltagon",
+    "Youtube":" YannD YDeltagon",
+    "Discord":" YannD / YDeltagon#2432",
 }
-### 
+### User enter his name
 your_name = input("What is your name ? ")
+
+### List for future
 welcome = ["Welcome ", "Hello ", "Surprise ", "Heyheyhey "]
 wlc = ["Hey ", "Ho ", "Hi ", "YoLo "]
 
@@ -20,10 +25,14 @@ nbwlc = random.randint(0,3)
 import random
 nbwelcome = random.randint(0,3)
 
-### Print messages
+### Print simple messages
 print(wlc[nbwlc] + your_name + ".")
-print()
 print(welcome[nbwelcome] + your_name + " on my first Python code !")
+print()
+
+### Print a message depending on the response
+my_social = input("What network of" + owner["creator"] + " do you want to know ? ")
+print("My " + my_social + " is" + owner[my_social])
 
 ### Debug the random numbers
 logging.debug(nbwlc)
