9dd9e5f66f9e4f3eb827b9aa3bfcc721800191f1
YDeltagon <git@ydeltagon.fr>
2021-08-04 13:07:35 +0200

    V0.4.0

--- Main.py
+++ Main.py
@@ -1,57 +1,207 @@
-### Debug TRUE
+### Import
 import logging, sys
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+import random
+
+### Base
+welcome = ["Welcome ", "Hello ", "Surprise ", "Heyheyhey "]
+wlc = ["Hey ", "Ho ", "Hi ", "YoLo "]
+nbwlc = random.randint(0,3)
+nbwelcome = random.randint(0,3)
+
+### First fonc loop condition
+def fonc_user_count():
+    in_user_count = 0
+    while in_user_count <= 0:
+        in_user_count = input("Numbers of players ? ")
+        try:
+            in_user_count = int(in_user_count)
+        except:
+            print("ERROR " + in_user_count + ": Pls, enter a valide numbers")
+            in_user_count = 0
+        if in_user_count > 4 :
+            print("max 4 players")
+            in_user_count = 0
+        elif in_user_count <= 0 :
+            print("Pls, enter a valide numbers")
+            in_user_count = 0
+    return in_user_count
+
+
+def fonc_user_name(user_nmb):
+    in_user_name = ""
+    while in_user_name == "":
+        in_user_name = input("Player " + user_nmb + ", what is your name ? ")
+    return in_user_name
+
+
+def fonc_user_age(user_name):
+    in_user_age = 0
+    while in_user_age == 0:
+        in_user_age = input(user_name + ", what is your age ? ")
+        try:
+            in_user_age = int(in_user_age)
+        except:
+            in_user_age = 0
+            print("ERROR " + user_name + ": Pls, enter a valide numbers")
+    return in_user_age
+
+
+def fonc_user_select():
+    user_exist = 0
+    global user_age1
+    global user_age2
+    global user_age3
+    global user_age4
+    global user_name1
+    global user_name2
+    global user_name3
+    global user_name4
+    global user_count
+    if user_count == 1 :
+        while user_exist == 0:
+            in_user_exist = input("What is your player name ? ")
+            if in_user_exist == user_name1 :
+                user_exist = 1
+            else:
+                print("ERROR " + in_user_exist + " not exist")
+    elif user_count == 2 :
+        while user_exist == 0:
+            in_user_exist = input("What is your player name ? ")
+            if in_user_exist == user_name1 :
+                user_exist = 1
+            elif in_user_exist == user_name2 :
+                user_exist = 2
+            else:
+                print("ERROR " + in_user_exist + " not exist")
+    elif user_count == 3 :
+        while user_exist == 0:
+            in_user_exist = input("What is your player name ? ")
+            if in_user_exist == user_name1 :
+                user_exist = 1
+            elif in_user_exist == user_name2 :
+                user_exist = 2
+            elif in_user_exist == user_name3 :
+                user_exist = 3
+            else:
+                print("ERROR " + in_user_exist + " not exist")
+    elif user_count == 4 :
+        while user_exist == 0:
+            in_user_exist = input("What is your player name ? ")
+            if in_user_exist == user_name1 :
+                user_exist = 1
+            elif in_user_exist == user_name2 :
+                user_exist = 2
+            elif in_user_exist == user_name3 :
+                user_exist = 3
+            elif in_user_exist == user_name4 :
+                user_exist = 4
+            else:
+                print("ERROR " + in_user_exist + " not exist")
+    else:
+        print("WTF 1")
+    return user_exist
+
+
+def fonc_print_user_select(name, age):
+    print()
+    print(welcome[nbwelcome] + name + " on my first Python code !")
+    print(name + ", you have " + str(age))
+    if age == 17:
+        print("Next years, mother fucker !")
+    elif age == 18:
+        print("WELCOME !!!")
+    elif 12 <= age < 18:
+        print("OMG!")
+    elif age == 1 or age ==2:
+        print("Liars")
+    elif age > 110:
+        print("Liars")
+    elif age > 80:
+        print("Boomers")
+    elif age > 50:
+        print("Keep calm")
+    elif age > 18:
+        print("You are Adult")
+    else:
+        print("You are Young")
+
 
 ### First Dict
 owner = {
-    "crea_date":"2021-08-02",
-    "creator":" YDeltagon",
     "Twitch":" @YDeltagon", 
     "Twitter":" @YDeltagon",
     "Youtube":" YannD YDeltagon",
     "Discord":" YannD / YDeltagon#2432",
 }
-### 
-in_user_name = input("What is your name ? ")
-in_user_age = 0
-while not in_user_age > 0:
-    in_user_age = input("What is your age ? ")
-    try:
-        in_user_age = int(in_user_age)
-    except:
-        in_user_age = 0
-        print("ERROR : Pls, enter a valide numbers")
 
-welcome = ["Welcome ", "Hello ", "Surprise ", "Heyheyhey "]
-wlc = ["Hey ", "Ho ", "Hi ", "YoLo "]
 
-### Generate a Random number for the firts list
-import random
-nbwlc = random.randint(0,3)
-### Generate a Random number for the second list
-import random
-nbwelcome = random.randint(0,3)
+### Question
+user_count = fonc_user_count()
+if user_count == 1:
+    user_name1 = fonc_user_name("1")
+elif user_count == 2:
+    user_name1 = fonc_user_name("1")
+    user_name2 = fonc_user_name("2")
+elif user_count == 3:
+    user_name1 = fonc_user_name("1")
+    user_name2 = fonc_user_name("2")
+    user_name3 = fonc_user_name("3")
+elif user_count == 4:
+    user_name1 = fonc_user_name("1")
+    user_name2 = fonc_user_name("2")
+    user_name3 = fonc_user_name("3")
+    user_name4 = fonc_user_name("4")
+
+if user_count == 1:
+    user_age1 = fonc_user_age(user_name1)
+elif user_count == 2:
+    user_age1 = fonc_user_age(user_name1)
+    user_age2 = fonc_user_age(user_name2)
+elif user_count == 3:
+    user_age1 = fonc_user_age(user_name1)
+    user_age2 = fonc_user_age(user_name2)
+    user_age3 = fonc_user_age(user_name3)
+elif user_count == 4:
+    user_age1 = fonc_user_age(user_name1)
+    user_age2 = fonc_user_age(user_name2)
+    user_age3 = fonc_user_age(user_name3)
+    user_age4 = fonc_user_age(user_name4)
+
+user_select = fonc_user_select()
+
+
+"""
+### First For
+for i in range(0, str(user_count)):
+    fonc_user_name(str(i+1))
+"""
+
 
 ### Print messages
-print(wlc[nbwlc] + in_user_name + ".")
-print()
-print(welcome[nbwelcome] + in_user_name + " on my first Python code !")
-print("You have " + str(in_user_age - 142))
+if user_select == 1:
+    fonc_print_user_select(user_name1, user_age1)
+elif user_select == 2:
+    fonc_print_user_select(user_name2, user_age2)
 
+"""
 ### Print a message depending on the response
 my_social = input("What network of" + owner["creator"] + " do you want to know ? ")
 print("My " + my_social + " is" + owner[my_social])
+"""
 
 ### Debug the random numbers
-logging.debug(type(in_user_age))
+logging.debug(type(user_age1))
+logging.debug(type(user_age2))
 logging.debug(nbwlc)
 logging.debug(nbwelcome)
 
-
-# in_user_social = "na"
-# while not in_user_social == exit:
-#     in_user_social = input("Which social network do you want to know ? ")
-#         try:
-#             "social"
-#         except:
-#             print("ERROR : Pls, enter a valide network")
+"""
+in_user_social = "na"
+while not in_user_social == exit:
+    in_user_social = input("Which social network do you want to know ? ")
+        try:
+            "social"
+        except:
+            print("ERROR : Pls, enter a valide network")
+"""
--- README.md
+++ README.md
@@ -2,11 +2,26 @@
 It's very simple, but, it's my "tuto" files  
 First GITHUB usage also.
 
+## V0.4.x
+- V0.4.0
+    - Multiple users creation
+    - Multiple name and age
+    - Selection of users (1 to 4)
+    - Limitation of 1 to 4 users
+    - Print a text dependent of your age
+    
 ## V0.3.x
-- V0.3.0
-    - Add a Question on my script to respond with my social networks
+- V0.3.2
+    - First fonc (def)
+    - First loop (for)
+    - First condition (if/elif/else/and/or/)
+    - First loop condition (while)
+    - Add two users creation
+    - Add a select of user
 - V0.3.1
     - Check data enter for Age and check in loop (first while usage)
+- V0.3.0
+    - Add a Question on my script to respond with my social networks
 
 ## V0.2.x
 - V0.2.1 
